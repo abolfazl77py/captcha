27bb7d3e0c758ce8b08bef7e5c0ac69d26b2df30
abolfazl <ab7hidden@gmail.com>
2022-02-18 21:07:12 +0330

    some change in codes

--- captcha.py
+++ captcha.py
@@ -1,41 +1,49 @@
 """generate captcha image"""
+
 from PIL import Image, ImageDraw, ImageFont
 import os
 from random import randint, choice
 
-def generate_captcha(path, size):
+
+def rand_color():
+    return randint(0,999)
+
+
+
+def generate_captcha(path, size="""this is for size"""):
     imag = Image.new("RGB",size,color="white")
     imag.save(path, "png")
     capt_img = Image.open(path)
     d1 = ImageDraw.Draw(capt_img)
     font= ImageFont.truetype("img/RansomRegular.ttf", 35)
-    d1.text((65, 10), "Dr5Ey6", fill= (100,50,0), font=font)
+    d1.text((65, 10), "Dr5Ey6", fill= (0,100,0), font=font)
     capt_img.save(path)
 
-def math_captcha():
+def mathematical_operations():
     randnum2, randnum1 = randint(0,99), randint(0,99)
     m = ('s', 't')
     math_choice = choice(m)
-    if math_choice == 's':
-        print(f"{randnum1} + {randnum2} :{randnum1 + randnum2}")
+    if math_choice == 't':
+        return f"{randnum1} + {randnum2}"
     else:
-        print(f"{randnum1} - {randnum2} :{randnum1 - randnum2}")
+        return f"{randnum1} - {randnum2}"
 
-math_captcha()
-# generate_captcha("res.png", (200,50))
-# imag = Image.new("RGB",(400,200),color="white")
-# imag.save('img/image.png', "png")
-# img = Image.open("img/image.png")
-# d1 = ImageDraw.Draw(img)
-# font= ImageFont.truetype("img/RansomRegular.ttf", 30)
-# d1.text((65, 10), "Dr5Ey6", fill= (255,0,0), font=font)
-# img.show()
-# img.save("img/result.png")
 
-# def generate_captcha():
-#     img.save('image.png', "png")
+def generate_mathematical_captcha(path, size):
+    """
+    path
+    type: str | file direction and its name
 
-# try:
-#     os.chdir("img")
-# except FileNotFoundError:
-#     os.mkdir("img")
+    size
+    type: tuple | size of image you want
+    """
+    number = mathematical_operations()
+    imag = Image.new("RGB",size,color="white")
+    imag.save(path, "png")
+    capt_img = Image.open(path)
+    d1 = ImageDraw.Draw(capt_img)
+    font= ImageFont.truetype("img/RansomRegular.ttf", 35)
+    d1.text((65, 10), str(number), fill= (200, 40, 0), font=font)
+    capt_img.save(path)
+
+generate_mathematical_captcha()
