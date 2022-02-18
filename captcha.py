aec507081d22ee5e99901d32ae91ffad89732e58
abolfazl <ab7hidden@gmail.com>
2022-02-18 23:44:40 +0330

    complate codes

--- captcha.py
+++ captcha.py
@@ -1,38 +1,42 @@
 """generate captcha image"""
 
 from PIL import Image, ImageDraw, ImageFont
-import os
+import string
 from random import randint, choice
 
 
 def rand_color():
-    return randint(0,999)
+    return choice(["red", "blue", "gray", "green", "black", "pink"])
+
+def rand_str():
+    return ''.join(choice(string.digits + string.ascii_letters) for i in range(6))
 
 
-
-def generate_captcha(path, size="""this is for size"""):
+def captcha_generator(path, size=(250, 50)):
     imag = Image.new("RGB",size,color="white")
     imag.save(path, "png")
     capt_img = Image.open(path)
     d1 = ImageDraw.Draw(capt_img)
-    font= ImageFont.truetype("img/RansomRegular.ttf", 35)
-    d1.text((65, 10), "Dr5Ey6", fill= (0,100,0), font=font)
+    font= ImageFont.truetype("font/RansomRegular.ttf", 35)
+    d1.text((65, 10),rand_str(), fill=rand_color(), font=font)
     capt_img.save(path)
 
 def mathematical_operations():
-    randnum2, randnum1 = randint(0,99), randint(0,99)
-    m = ('s', 't')
+    randnum1, randnum2 = randint(0,99), randint(0,99)
+    m = ('sum', 'subtract')
     math_choice = choice(m)
-    if math_choice == 't':
+    if math_choice == 'sum':
         return f"{randnum1} + {randnum2}"
     else:
-        return f"{randnum1} - {randnum2}"
+        if randnum1 >= randnum2:
+            return f"{randnum1} - {randnum2}"
+        return f"{randnum2} - {randnum1}"
 
 
-def generate_mathematical_captcha(path, size):
+def mathematical_captcha(path, size= (250, 50)):
     """
     path
-    type: str | file direction and its name
+    type: str | file direction and its name (.png)
 
     size
     type: tuple | size of image you want
@@ -42,8 +46,7 @@ def generate_mathematical_captcha(path, size):
     imag.save(path, "png")
     capt_img = Image.open(path)
     d1 = ImageDraw.Draw(capt_img)
-    font= ImageFont.truetype("img/RansomRegular.ttf", 35)
-    d1.text((65, 10), str(number), fill= (200, 40, 0), font=font)
+    font= ImageFont.truetype("font/RansomRegular.ttf", 35)
+    d1.text((65, 1), str(number), fill= rand_color(), font=font)
     capt_img.save(path)
 
-generate_mathematical_captcha()
